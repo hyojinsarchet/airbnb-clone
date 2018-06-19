163c33ff3f203e2de9904b61f273e82cdcf456ae
hyojin.sarchet <hyojin5931@hanmail.net>
2018-06-19 17:29:02 -0400

    updating all files to github

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Flat from "./components/Flat";
+
+class App extends Component {
+  render() {
+    const flat = {
+      id: 148,
+      name: "Trendy Apt in Buttes Montmartre",
+      imageUrl:
+        "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
+      price: 200,
+      priceCurrency: "EUR",
+      lat: 48.885707,
+      lng: 2.343543
+    };
+
+    return (
+      <div className="app">
+        <div className="main">
+          <div className="search" />
+          <div className="flats">
+            <Flat flat={flat} />
+            <Flat flat={flat} />
+            <Flat flat={flat} />
+          </div>
+        </div>
+        <div className="map" />
+      </div>
+    );
+  }
+}
+
+export default App;
